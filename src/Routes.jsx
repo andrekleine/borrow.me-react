a6b2ef479bc41ff0a487eca1c554567b373d28c7
André Kleine <andrekleine@gmail.com>
2021-12-04 10:52:20 -0300

    Create Protected Route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,10 +4,10 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './components/pages/auth/Login';
 import Register from './components/pages/auth/Register';
 
+import ProtectedRoute from './components/misc/ProtectedRoute';
+
 // import MyProjects from './components/pages/MyProjects/MyProjects';
 // import ProjectDetails from './components/pages/ProjectDetails/ProjectDetails';
-
-// import ProtectedRoute from './components/miscelaneous/ProtectedRoute/ProtectedRoute';
 
 const appRoutes = () => {
   const verifyLoggedUser = () => {
@@ -26,6 +26,11 @@ const appRoutes = () => {
     <Routes>
       <Route path="/" element={<Login loginUser={loginUser} />} />
       <Route path="/register" element={<Register />} />
+
+      <Route
+        path="/books"
+        element={<ProtectedRoute isLogged={isUserLogged} Page={Home} />}
+      />
     </Routes>
   );
 };
